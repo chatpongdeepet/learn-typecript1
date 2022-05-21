d7195e94733e3408be05c5ae8f350d9d277fcb6f
theDcreative <tangdesign.sci@gmail.com>
2022-05-21 20:00:17 +0700

    Create tsconfig.json and debuging

--- app.ts
+++ app.ts
@@ -1,3 +1,6 @@
 let text: string;
+let text1: string;
 text = 'Hello World';
+text1 = 'Chatpong Deepet';
 console.log(text);
+console.log(text1);
---
+++ out/app.js
@@ -0,0 +1,7 @@
+var text;
+var text1;
+text = 'Hello World';
+text1 = 'Chatpong Deepet';
+console.log(text);
+console.log(text1);
+//# sourceMappingURL=app.js.map
